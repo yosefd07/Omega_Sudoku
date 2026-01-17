225cbef8f536d7b4a2fc1ccf009e4109a96af87c
yosefd07 <yosefdavidyan@gmail.com>
2026-01-18 00:12:57 +0200

    Added Board class and defined board methods

--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace SudokuSolver
 {
     internal class SudokuBoard
     {
+        public const int ROWS = 9;
+        public const int COLUMNS = 9;
+
+        int[,] Grid = new int[ROWS, COLUMNS];
+
+        public int GetCell(int x, int y)
+        {
+            return Grid[x, y];
+        }
+        public void SetCell(int x, int y, int value)
+        {
+            Grid[x, y] = value;
+        }
+
+        public bool IsEmpty(int x, int y)
+        {
+            if (Grid[x, y] == 0)
+                return true;
+            return false;
+        }
     }
 }
