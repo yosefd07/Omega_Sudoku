9d8712589bc0af055c999bea3c3ce6ae1fc926ec
yosefd07 <yosefdavidyan@gmail.com>
2026-01-18 00:34:52 +0200

    Implemented initial Parser logic

--- SudokuSolver/SudokuParser.cs
+++ SudokuSolver/SudokuParser.cs
@@ -8,5 +8,29 @@ namespace SudokuSolver
 {
     internal class SudokuParser
     {
+        public SudokuBoard Parse(string input)
+        {
+            if (input == null)
+                throw new ArgumentException("Input cannot be null");
+
+            if (input.Length != 81)
+                throw new ArgumentException("Input must contain exactly 81 characters");
+
+            SudokuBoard board = new SudokuBoard();
+
+            for (int i = 0; i < 81; i++)
+            {
+                if (input[i] < '0' || input[i] > '9')
+                    throw new ArgumentException("Input contains invalid characters");
+
+                int row = i / 9;
+                int col = i % 9;
+                int value = input[i] - '0';
+
+                board.SetCell(row, col, value);
+            }
+
+            return board;
+        }
     }
 }
