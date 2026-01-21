de1bfbdeb852e640fd4718609ba67c00120c2e8a
yosefd07 <yosefdavidyan@gmail.com>
2026-01-21 14:34:11 +0200

    add the solver starter with the base case

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace SudokuSolver
 {
-    internal class SudokuSolver
+    namespace SudokuSolver
     {
+        internal class SudokuSolver
+        {
+            private bool IsSafe(SudokuBoard board, int row, int col, int num)
+            {
+                // TODO
+            }
+
+            private bool FindEmptyCell(SudokuBoard board, out int row, out int col)
+            {
+                for (row = 0; row < 9; row++)
+                {
+                    for (col = 0; col < 9; col++)
+                    {
+                        if (board.IsEmpty(row, col))
+                            return true;
+                    }
+                }
+
+                row = -1;
+                col = -1;
+                return false;
+            }
+            public bool Solve(SudokuBoard board)
+            {
+                int row, col;
+
+                if (!FindEmptyCell(board, out row, out col))
+                    return true;
+                
+                for (int num = 1; num <= 9; num++)
+                {
+                    board.SetCell(row, col, num);
+
+                    if (Solve(board))
+                        return true;
+
+                    board.SetCell(row, col, 0); // Backtrack
+                }
+
+
+                return false;
+            }
+        }
     }
 }
+
