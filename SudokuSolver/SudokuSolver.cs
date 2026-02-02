1987f26452fda4464c5089bf5e767f2b006d7c47
yosefd07 <yosefdavidyan@gmail.com>
2026-02-03 01:10:59 +0200

    Implement SudokuSolver with backtracking algorithm

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -6,50 +6,73 @@ using System.Threading.Tasks;
 
 namespace SudokuSolver
 {
-    namespace SudokuSolver
+    internal class SudokuSolver
     {
-        internal class SudokuSolver
+        private bool IsSafe(SudokuBoard board, int row, int col, int num)
         {
-            private bool IsSafe(SudokuBoard board, int row, int col, int num)
+            for (int c = 0; c < 9; c++)
             {
-                // TODO
+                if (board.GetCell(row, c) == num)
+                    return false;
             }
 
-            private bool FindEmptyCell(SudokuBoard board, out int row, out int col)
+            for (int r = 0; r < 9; r++)
             {
-                for (row = 0; row < 9; row++)
+                if (board.GetCell(r, col) == num)
+                    return false;
+            }
+
+            int startRow = (row / 3) * 3;
+            int startCol = (col / 3) * 3;
+
+            for (int r = startRow; r < startRow + 3; r++)
+            {
+                for (int c = startCol; c < startCol + 3; c++)
                 {
-                    for (col = 0; col < 9; col++)
-                    {
-                        if (board.IsEmpty(row, col))
-                            return true;
-                    }
+                    if (board.GetCell(r, c) == num)
+                        return false;
                 }
-
-                row = -1;
-                col = -1;
-                return false;
             }
-            public bool Solve(SudokuBoard board)
-            {
-                int row, col;
 
-                if (!FindEmptyCell(board, out row, out col))
-                    return true;
-                
-                for (int num = 1; num <= 9; num++)
+            return true;
+        }
+
+        private bool FindEmptyCell(SudokuBoard board, out int row, out int col)
+        {
+            for (row = 0; row < 9; row++)
+            {
+                for (col = 0; col < 9; col++)
+                {
+                    if (board.IsEmpty(row, col))
+                        return true;
+                }
+            }
+
+            row = -1;
+            col = -1;
+            return false;
+        }
+        public bool Solve(SudokuBoard board)
+        {
+            int row, col;
+
+            if (!FindEmptyCell(board, out row, out col))
+                return true;
+
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsSafe(board, row, col, num))
                 {
                     board.SetCell(row, col, num);
 
                     if (Solve(board))
                         return true;
 
-                    board.SetCell(row, col, 0); // Backtrack
+                    board.SetCell(row, col, 0);
                 }
-
-
-                return false;
             }
+
+            return false;
         }
     }
 }
